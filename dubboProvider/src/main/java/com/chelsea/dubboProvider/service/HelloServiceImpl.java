28f96f72f234dd7af52a7dd983e5cc79252eba29
baojun <baojun_a@aspirecn.com>
2018-04-25 21:23:55 +0800

    修改服务名

--- dubboProvider/src/main/java/com/chelsea/dubboProvider/service/HelloServiceImpl.java
+++ dubboProvider/src/main/java/com/chelsea/dubboProvider/service/HelloServiceImpl.java
@@ -2,14 +2,14 @@ package com.chelsea.dubboProvider.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.chelsea.dubboInterface.HelloService;
 import com.chelsea.dubboProvider.dao.HelloDao;
 
-@Service
+@Service("helloService")
 public class HelloServiceImpl implements HelloService {
-    
+
     @Autowired
     private HelloDao helloDao;
 
