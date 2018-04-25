9e5302735d16f1ee6557471930b14012b9add0e6
baojun <baojun_a@aspirecn.com>
2018-04-25 21:24:16 +0800

    新增异步测试

--- dubboConsumer/src/test/java/com/chelsea/dubboConsumer/DubboConsumerTest.java
+++ dubboConsumer/src/test/java/com/chelsea/dubboConsumer/DubboConsumerTest.java
@@ -1,24 +1,35 @@
 package com.chelsea.dubboConsumer;
 
+import java.util.concurrent.Future;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class DubboConsumerTest{
+import com.alibaba.dubbo.rpc.RpcContext;
 
-	ApplicationContext ctx = null;
-	
-	@Before
-	public void init(){
-		ctx = new ClassPathXmlApplicationContext("classpath:application.xml");
-	}
-	
-	@Test
-	public void testConsumer() throws Exception{
-		HelloServiceConsumer service = (HelloServiceConsumer)ctx.getBean("helloServiceConsumer");
-		String result = service.sayHello("张三");
-		System.out.print(result);
-	}
+public class DubboConsumerTest {
+
+    ApplicationContext ctx = null;
+
+    @Before
+    public void init() {
+        ctx = new ClassPathXmlApplicationContext("classpath:application.xml");
+    }
+
+    /**
+     * 异步调用
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testAsync() throws Exception {
+        HelloServiceConsumer service = (HelloServiceConsumer) ctx.getBean("helloServiceConsumer");
+        String result = service.sayHello("张三");
+        Future<String> future = RpcContext.getContext().getFuture();
+        result = future.get();
+        System.out.print(result);
+    }
 
 }
