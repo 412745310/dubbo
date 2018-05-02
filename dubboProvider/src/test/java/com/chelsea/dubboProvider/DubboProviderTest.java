2d060d7fbc0bceb760b669c4dc57532bb0785dbf
baojun <baojun_a@aspirecn.com>
2018-05-02 22:55:42 +0800

    删除没有用的引入

--- dubboProvider/src/test/java/com/chelsea/dubboProvider/DubboProviderTest.java
+++ dubboProvider/src/test/java/com/chelsea/dubboProvider/DubboProviderTest.java
@@ -1,9 +1,7 @@
 package com.chelsea.dubboProvider;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.alibaba.dubbo.container.Main;
 
